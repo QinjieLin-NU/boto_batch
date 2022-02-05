5988b448d1b33c0a8f34afd74bf9efb15a799cab
QinjieLin-NU <qinjielin2018@u.northwestern.edu>
2022-02-04 18:33:42 -0600

    update

--- cluster/util_batch.py
+++ cluster/util_batch.py
@@ -1,25 +1,16 @@
-import yaml
-import json
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--user', type=str, help='userId path', required=True)
 args = parser.parse_args()
 
-def load_config(file_path):
-    with open(file_path) as f:
-        loaded_config = yaml.safe_load(f)
-    return loaded_config
+with open("config/example_job.yaml",'r') as f:
+    x = f.readlines()
 
-def save_config(file_path,target_config):
-    with open(file_path, 'w') as file:
-        documents = yaml.dump(target_config, file, default_flow_style=False, sort_keys=False)
-
-def generate_exmaple(efs_path):
-    config_path = './config/example_job.yaml'
-    target_config = load_config(config_path)
-    target_config['mount_config'][0]['efs_path'] = efs_path
-    
-    config_path = './config/example_job.yaml'
-    save_config(config_path,target_config)
-
-generate_exmaple(args.user)
+user=args.user
+with open("config/example_job.yaml",'w') as f:
+    for i in x:
+        if 'efs_path' in i:
+            i = f'  efs_path: {user}\n'
+        if 'job_name' in i:
+            i = f'job_name: job{user}\n'
+        f.write(i)
